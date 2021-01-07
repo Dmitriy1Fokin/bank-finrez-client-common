9821042dadf77d77adaa45808ee2350583901193
Dmitriy Fokin <dmitrii.fokin@aisa.ru>
2021-01-07 15:02:46 +0300

    first commit. init empty project

---
+++ src/main/kotlin/ru/fdo/bank/finrez/clientservicecommon/ClientServiceCommonApplication.kt
@@ -0,0 +1,11 @@
+package ru.fdo.bank.finrez.clientservicecommon
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ClientServiceCommonApplication
+
+fun main(args: Array<String>) {
+    runApplication<ClientServiceCommonApplication>(*args)
+}
---
+++ src/test/kotlin/ru/fdo/bank/finrez/clientservicecommon/ClientServiceCommonApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.fdo.bank.finrez.clientservicecommon
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ClientServiceCommonApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
