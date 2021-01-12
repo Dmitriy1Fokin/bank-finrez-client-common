b9de085856c6ae5037ccb2e7e9ff4e3ecdb3eb21
Dmitriy Fokin <fokinds1@gmail.com>
2021-01-12 14:03:38 +0300

    added field in FindClientByParamsQuery

--- src/main/kotlin/ru/fdo/bank/finrez/clientservicecommon/coreapi/query/FindClientByParamsQuery.kt
+++ src/main/kotlin/ru/fdo/bank/finrez/clientservicecommon/coreapi/query/FindClientByParamsQuery.kt
@@ -1,3 +1,5 @@
 package ru.fdo.bank.finrez.clientservicecommon.coreapi.query
 
-data class FindClientByParamsQuery(val searchParameters: Map<String, String>)
+import org.springframework.data.domain.Pageable
+
+data class FindClientByParamsQuery(val searchParameters: Map<String, String>, val pageable: Pageable)
